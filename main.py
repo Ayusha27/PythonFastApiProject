048f7e8e713c64aa9ee83abe8269709557382659
Ayusha Nayak <ayushanayak@Ayushas-MacBook-Air.local>
2026-02-14 01:43:45 +0530

    added comments to run the uvicorn app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+# Run the FastAPI application using Uvicorn
+# uvicorn src.app:app --reload
 if __name__ == "__main__":
     uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
